6aab82168223eff87e9364c09fe1bbd5c613fbaa
jiajialing <jiajialing@gmail.com>
2018-05-11 00:01:41 +1000

    fe

--- WindowsFormsApp1/Form2.Designer.cs
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -29,33 +29,48 @@
         private void InitializeComponent()
         {
             this.btn_back = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_back
             // 
-            this.btn_back.Location = new System.Drawing.Point(12, 12);
+            this.btn_back.Location = new System.Drawing.Point(433, 243);
             this.btn_back.Name = "btn_back";
-            this.btn_back.Size = new System.Drawing.Size(210, 86);
+            this.btn_back.Size = new System.Drawing.Size(101, 26);
             this.btn_back.TabIndex = 0;
             this.btn_back.Text = "Back";
             this.btn_back.UseVisualStyleBackColor = true;
             this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome to Addition";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(234, 110);
+            this.ClientSize = new System.Drawing.Size(546, 281);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_back);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Label label1;
     }
 }
